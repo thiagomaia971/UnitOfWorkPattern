55b9e9fcfe15c96d231a5d09414d4abe19dd8a0e
Thiago Maia da Costa <thiago.maia971@hotmail.com>
2020-02-18 21:13:31 -0300

    Criando nova solution

---
+++ _UnitOfWorkPattern/UnitOfWorkPattern.Infr/MyContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitOfWorkPattern.Repositories;
+
+namespace UnitOfWorkPattern.Infr
+{
+    public class MyContext : DbContext
+    {
+        // Yours DbSet
+        // public DbSet<Cliente> Clientes { get; set; }
+
+        public MyContext(): base("name=DefaultConnetion")
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+            this.Configuration.ProxyCreationEnabled = false;
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Yours TypeConfiguration
+            //modelBuilder.Configurations.Add(new ClienteEntityTypeConfiguration());
+        }
+
+    }
+}
---
+++ _UnitOfWorkPattern/UnitOfWorkPattern.Infr/Repositories/Repository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitOfWorkPattern.Repositories;
+
+namespace UnitOfWorkPattern.Infr.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly DbContext Context;
+        private DbSet<T> dbSet;
+
+        public Repository(DbContext Context)
+        {
+            this.Context = Context;
+            dbSet = this.Context.Set<T>();
+        }
+
+        public void Add(T Entity)
+        {
+            dbSet.Add(Entity);
+        }
+
+        public void Delete(int id)
+        {
+            T Entity = this.GetSingle(id);
+            this.Delete(Entity);
+        }
+
+        public void Delete(T Entity)
+        {
+            dbSet.Remove(Entity);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return dbSet.ToList();
+        }
+
+        public IEnumerable<T> GetAll(Func<T, bool> predicate)
+        {
+            return dbSet.Where(predicate).ToList();
+        }
+
+        public T GetSingle(int id)
+        {
+            return dbSet.Find(id);
+        }
+
+    }
+}
